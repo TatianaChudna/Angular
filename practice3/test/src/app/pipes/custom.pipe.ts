82f80441ac8fc191a9d82a1565608a0c20912253
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-11 17:12:37 +0200

    Fix custom pipe

--- practice3/test/src/app/pipes/custom.pipe.ts
+++ practice3/test/src/app/pipes/custom.pipe.ts
@@ -1,12 +1,20 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Pipe({
   name: 'custom'
 })
 export class CustomPipe implements PipeTransform {
+  public formatsDateTest: string[] = [
+    'M/d/yy, h:mm a',
+    ' MMM d, y, h:mm:ss a',
+    ' EEEE, MMMM d, y, h:mm:ss a zzzz'
+  ];
 
   transform(value: any, ...args: any[]): any {
-    const customDate = new Date();
-    return customDate;
+    const datePipe = new DatePipe('en-US');
+    // @ts-ignore
+    value = datePipe.transform(value, this.formatsDateTest);
+    return value;
   }
 }
